5b0485e283e5086b012eeb74e0cdb5a304a4884d
xstephenhu98 <xstephenhu98@gmail.com>
2015-11-04 18:05:17 -0500

    Installed polymer, Updated login form

--- auth.py
+++ auth.py
@@ -1,11 +1,11 @@
 from urllib2 import Request, urlopen
 from urllib import urlencode
 
-values = urlencode(client_id=0ebc6165a0d7516fd7a5
-	&client_secret=9251f07bb37c6bbee4afb787674d01b745a98ead
-	&grant_type=password
-	&username=xhu5000
-	&password=50b864cd126e82be40e9a0f678fc5c3e)
+values = urlencode(client_id="0ebc6165a0d7516fd7a5"
+	&client_secret="9251f07bb37c6bbee4afb787674d01b745a98ead"
+	&grant_type="password"
+	&username="xhu5000"
+	&password="50b864cd126e82be40e9a0f678fc5c3e")
 
 headers = {
   'Content-Type': 'application/x-www-form-urlencoded'
